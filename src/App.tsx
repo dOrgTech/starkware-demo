4e41f997aba7ffb21b82c1ff2ecb39fbe8aba436
Elio <eliobricenov@gmail.com>
2021-05-10 23:49:44 -0400

    chore: move provider declaration to index file

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-import React from "react";
-import { Box, styled, ThemeProvider, Typography } from "@material-ui/core";
-import { theme } from "theme/theme";
+import React from 'react';
+import { Box, styled, Typography } from '@material-ui/core';
 
 const Layout = styled(Box)({
-  background: "linear-gradient(179.61deg, #101062 0.34%, #12125D 22.07%, #141457 41.21%, #151552 72.76%, #0D0D46 99.66%);",
-  width: "100%",
-  minHeight: "100vh"
-})
+	background:
+		'linear-gradient(179.61deg, #101062 0.34%, #12125D 22.07%, #141457 41.21%, #151552 72.76%, #0D0D46 99.66%);',
+	width: '100%',
+	minHeight: '100vh',
+});
 
 function App() {
-  return (
-    <>
-      <head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <ThemeProvider theme={theme}>
-        <Layout>
-          <Typography variant="body1">
-            Hello
-          </Typography>
-        </Layout>
-      </ThemeProvider>
-      
-    </>
-  );
+	return (
+		<Layout>
+			<Typography variant="body1">Hello</Typography>
+		</Layout>
+	);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { ThemeProvider } from '@material-ui/core';
+
+import App from 'App';
+import { theme } from 'theme/theme';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
+	document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
