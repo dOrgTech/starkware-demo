c2c1608de5b44718b66ccc45abd7efd4bbfef424
Elio <eliobricenov@gmail.com>
2021-05-12 00:14:33 -0400

    wip: add tabs content

--- src/components/Mint.tsx
+++ src/components/Mint.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { Grid, Typography } from '@material-ui/core';
+import { ReactComponent as ArrowDown } from '../assets/icons/arrow-down.svg';
+import { ReactComponent as TokenLogo } from '../assets/tokens/token.svg';
 
 export const Mint = (): JSX.Element => {
-	return <h1>Mint</h1>;
+	return (
+		<Grid container>
+			<Grid item container xs>
+				<Grid item container spacing={1} xs={8} sm={6} alignItems="center">
+					<Grid item>
+						<TokenLogo height="48px" width="48px" />
+					</Grid>
+					<Grid item>
+						<Typography variant="h4">TK1</Typography>
+					</Grid>
+					<Grid item>
+						<ArrowDown width="10px" height="5px" />
+					</Grid>
+				</Grid>
+				<Grid container item xs alignItems="center">
+					<Typography variant="h4">100</Typography>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, styled, Container } from '@material-ui/core';
+import { ModeSelector } from '../components/ModeSelector';
+
+const ModeSelectorContainer = styled(Grid)({
+	marginTop: '10%',
+});
 
 export const Home = (): JSX.Element => {
 	return (
-		<Grid container justify="center">
-			<Typography>Starkware Labs</Typography>
-		</Grid>
+		<Container maxWidth="lg">
+			<Grid container justify="center">
+				<ModeSelectorContainer item xs={12} sm={8} md={6} lg={5}>
+					<ModeSelector />
+				</ModeSelectorContainer>
+			</Grid>
+		</Container>
 	);
 };
