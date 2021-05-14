49c37ce18c570da20d3741e0d30d779ee48091fd
Elio <eliobricenov@gmail.com>
2021-05-14 19:04:02 -0400

    feat: add state logic

--- src/components/Mint.tsx
+++ src/components/Mint.tsx
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DarkBox } from './common/DarkBox';
 import { TokenSelector } from './TokenSelector';
-import { TOKENS } from './Swap';
+import { Token } from '../models/token';
+import { ReactComponent as TokenSVG } from '../assets/tokens/token.svg';
+import { ReactComponent as Token2SVG } from '../assets/tokens/token2.svg';
+import { Grid, styled } from '@material-ui/core';
+import { NumericInput } from './NumericInput';
+
+const StyledInputContainer = styled(Grid)({
+	padding: '0 0 0 12px',
+});
+
+const TOKENS: Token[] = [
+	{
+		id: '1',
+		name: 'Token 1',
+		symbol: 'TK1',
+		icon: TokenSVG,
+		price: '2',
+	},
+	{
+		id: '2',
+		name: 'Token 2',
+		symbol: 'TK2',
+		icon: Token2SVG,
+		price: '1',
+	},
+];
 
 export const Mint = (): JSX.Element => {
+	const [token, setToken] = useState<Token>();
+	const [mintAmount, setMintAmount] = useState<string>();
+
 	return (
 		<DarkBox>
-			<TokenSelector
-				onChange={(e) => {
-					console.log(e);
-				}}
-				options={TOKENS}
-			/>
+			<Grid container alignItems="center">
+				<Grid item xs>
+					<TokenSelector value={token} options={TOKENS} onChange={(token) => setToken(token)} />
+				</Grid>
+				{token && (
+					<StyledInputContainer item xs>
+						<Grid container justify="flex-end" alignItems="center">
+							<NumericInput value={mintAmount} onChange={(change) => setMintAmount(change)} />
+						</Grid>
+					</StyledInputContainer>
+				)}
+			</Grid>
 		</DarkBox>
 	);
 };
