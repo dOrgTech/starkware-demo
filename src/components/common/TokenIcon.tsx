2d9b13a541256cab9118fb6b2ba323b23e371db7
Elio <eliobricenov@gmail.com>
2021-05-14 22:33:45 -0400

    fix: change prop name to clear react warning

--- src/components/common/TokenIcon.tsx
+++ src/components/common/TokenIcon.tsx
@@ -1,37 +1,36 @@
-import React from 'react';
-import { Avatar, AvatarProps, styled } from '@material-ui/core';
-import { useMemo } from 'react';
+import React from "react"
+import { Avatar, AvatarProps, styled } from "@material-ui/core"
+import { useMemo } from "react"
 
-export type LogoSize = 'default' | 'large';
+export type LogoSize = "default" | "large"
 
 export interface TokenIconProps extends AvatarProps {
-	Icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-	size?: LogoSize;
+  Icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>,
+  size?: LogoSize
 }
 
-const TokenLogoContainer = styled(Avatar)((props: { widthAndHeight: number }) => ({
-	width: props.widthAndHeight,
-	height: props.widthAndHeight,
-	borderRadius: '100%',
-	'& > *': {
+const TokenLogoContainer = styled(Avatar)((props: { dimension: number }) => ({
+  width: props.dimension,
+  height: props.dimension,
+  borderRadius: "100%",
+  '& > *': {
 		width: '100%',
 		height: '100%',
 	},
-}));
+}))
 
-export const TokenIcon: React.FC<TokenIconProps> = ({ Icon, size = 'default', ...props }) => {
-	const iconSize: number = useMemo(() => {
-		switch (size) {
-			case 'default':
-				return 26;
-			case 'large':
-				return 48;
-		}
-	}, [size]);
+export const TokenIcon: React.FC<TokenIconProps> = ({ Icon, size = "default", ...props }) => {
 
-	return (
-		<TokenLogoContainer widthAndHeight={iconSize} {...props}>
-			<Icon />
-		</TokenLogoContainer>
-	);
-};
+  const iconSize: number = useMemo(() => {
+    switch(size) {
+      case "default": return 26;
+      case "large": return 48;
+    }
+  }, [size])
+
+  return (
+    <TokenLogoContainer dimension={iconSize} {...props}>
+      <Icon />
+    </TokenLogoContainer>
+  )
+}
