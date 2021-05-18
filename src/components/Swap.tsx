8644f06fa3e8fa6f7aa5a8b786c448524d328f4a
Nestor Amesty <nestor09amesty@gmail.com>
2021-05-18 04:56:41 +0200

    Token Price Conversions/Rates (#14)
    * Added conversion rates and onChange input handlers for them
    
    * Math operations now in BigNumbers
    
    * Removed useCallback/useMemo where not necessary
    
    * chore: run prettier
    
    Co-authored-by: Elio <eliobricenov@gmail.com>

--- src/components/Swap.tsx
+++ src/components/Swap.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useCallback, useMemo } from 'react';
 import { Button, Grid, IconButton, styled, Typography } from '@material-ui/core';
-
+import { BigNumber } from '@ethersproject/bignumber';
 import { ReactComponent as SwapDirection } from 'assets/icons/swap-direction.svg';
 import { TokenSelector } from './TokenSelector';
 import { DarkBox } from './common/DarkBox';
 import { NumericInput } from './NumericInput';
-import { Token } from 'services/API/token/types';
+import { Token, TokenBalance } from 'services/API/token/types';
 import { useTokens } from 'services/API/token/hooks/useTokens';
 import { useTokenOptions } from 'services/API/token/hooks/useTokenOptions';
 import { useTokenBalances } from 'services/API/token/hooks/useTokenBalances';
@@ -66,28 +66,39 @@ export const Swap = (): JSX.Element => {
 	const [fromValue, setFromValue] = useState('');
 	const [toValue, setToValue] = useState('');
 	const { data: tokensData } = useTokens();
-
 	const [fromToken, setFromToken] = useState<Token | undefined>();
 	const [fromAmount, setFromAmount] = useState<string>();
 	const [toToken, setToToken] = useState<Token>();
 	const [toAmount, setToAmount] = useState<string>();
 	const { data: tokenBalances } = useTokenBalances();
 
-	const fromBalance = useMemo(() => {
-		if (!fromToken || !tokenBalances) {
-			return undefined;
+	const conversionRates = useMemo(() => {
+		if (fromToken && toToken) {
+			return {
+				from: BigNumber.from(toToken.price).div(BigNumber.from(fromToken.price)),
+				to: BigNumber.from(fromToken.price).div(BigNumber.from(toToken.price)),
+			};
 		}
 
-		return tokenBalances.find((tokenBalance) => tokenBalance.token.symbol === fromToken.symbol);
-	}, [tokenBalances, fromToken]);
+		return {
+			from: 1,
+			to: 1,
+		};
+	}, [fromToken, toToken]);
 
-	const toBalance = useMemo(() => {
-		if (!toToken || !tokenBalances) {
-			return undefined;
-		}
+	let fromBalance: TokenBalance | undefined;
 
-		return tokenBalances.find((tokenBalance) => tokenBalance.token.symbol === toToken.symbol);
-	}, [tokenBalances, toToken]);
+	if (fromToken && tokenBalances) {
+		fromBalance = tokenBalances.find(
+			(tokenBalance) => tokenBalance.token.symbol === fromToken.symbol,
+		);
+	}
+
+	let toBalance: TokenBalance | undefined;
+
+	if (toToken && tokenBalances) {
+		toBalance = tokenBalances.find((tokenBalance) => tokenBalance.token.symbol === toToken.symbol);
+	}
 
 	const handleSwitch = useCallback(() => {
 		setFromValue(toValue);
@@ -95,7 +106,10 @@ export const Swap = (): JSX.Element => {
 
 		setFromToken(toToken);
 		setToToken(fromToken);
-	}, [fromToken, fromValue, toToken, toValue]);
+
+		setFromAmount(toAmount);
+		setToAmount(fromAmount);
+	}, [fromAmount, fromToken, fromValue, toAmount, toToken, toValue]);
 
 	const handleFromTokenSelected = (token: Token) => {
 		if (toToken && token.symbol === toToken.symbol) {
@@ -104,6 +118,22 @@ export const Swap = (): JSX.Element => {
 
 		setFromAmount('0');
 		setFromToken(token);
+	};
+
+	const handleFromAmountChange = (amount: string) => {
+		setFromAmount(amount);
+
+		if (conversionRates) {
+			setToAmount(BigNumber.from(amount).mul(BigNumber.from(conversionRates.to)).toString());
+		}
+	};
+
+	const handleToAmountChange = (amount: string) => {
+		setToAmount(amount);
+
+		if (conversionRates) {
+			setFromAmount(BigNumber.from(amount).mul(BigNumber.from(conversionRates.from)).toString());
+		}
 	};
 
 	const options = useTokenOptions(fromToken, tokensData);
@@ -130,12 +160,14 @@ export const Swap = (): JSX.Element => {
 												'aria-label': 'amount of token to swap',
 											}}
 											value={fromAmount}
-											handleChange={(change) => setFromAmount(change)}
+											handleChange={(change) => handleFromAmountChange(change)}
 										/>
 									</Grid>
 									{fromBalance && (
 										<Grid item xs={6}>
-											<RoundedButton onClick={() => setFromAmount(fromBalance.amount)}>
+											<RoundedButton
+												onClick={() => handleFromAmountChange((fromBalance as TokenBalance).amount)}
+											>
 												Max
 											</RoundedButton>
 										</Grid>
@@ -156,7 +188,7 @@ export const Swap = (): JSX.Element => {
 				</StyledArrowsContainer>
 			</Grid>
 			<Grid item xs={12}>
-				<Labels leftText="From" rightText={toBalance ? `Balance: ${toBalance.amount}` : ''} />
+				<Labels leftText="To" rightText={toBalance ? `Balance: ${toBalance.amount}` : ''} />
 				<DarkBox>
 					<Grid container alignItems="center">
 						<Grid item xs aria-label="token to be swapped">
@@ -174,7 +206,7 @@ export const Swap = (): JSX.Element => {
 											'aria-label': 'amount of token to be swapped',
 										}}
 										value={toAmount}
-										handleChange={(change) => setToAmount(change)}
+										handleChange={(change) => handleToAmountChange(change)}
 									/>
 								</Grid>
 							</StyledInputContainer>
