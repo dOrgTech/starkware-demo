dfe03ed94e8352d7774d04cea29ddcbe69af09ff
Nestor Amesty <nestor09amesty@gmail.com>
2021-05-20 00:40:02 +0200

    Minting of the 2 tokens at the same time (#23)

--- src/components/Mint.tsx
+++ src/components/Mint.tsx
@@ -1,44 +1,111 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { DarkBox } from './common/DarkBox';
-import { TokenSelector } from './TokenSelector';
+import { SelectedToken } from './TokenSelector';
 
-import { Grid, styled } from '@material-ui/core';
+import { Button, Grid, styled } from '@material-ui/core';
 import { NumericInput } from './NumericInput';
 import { useTokens } from 'services/API/token/hooks/useTokens';
-import { Token } from 'models/Token';
-import { useTokenOptions } from 'services/API/token/hooks/useTokenOptions';
+import { ActionTypes, NotificationsContext } from 'context/notifications';
 
 const StyledInputContainer = styled(Grid)({
 	padding: '0 0 0 12px',
 });
 
+const StyledContainer = styled(Grid)({
+	'& > *': {
+		marginBottom: '30px',
+	},
+});
+
+const getMintFormError = (amount1?: string, amount2?: string): string | undefined => {
+	if (Number(amount1) > 1000 || Number(amount2) > 1000) {
+		return 'Cannot mint more than 1000';
+	}
+};
+
 export const Mint = (): JSX.Element => {
-	const [token, setToken] = useState<Token>();
-	const [mintAmount, setMintAmount] = useState<string>();
+	const [mintAmount1, setMintAmount1] = useState<string>('1');
+	const [mintAmount2, setMintAmount2] = useState<string>('1000');
+	const { dispatch } = useContext(NotificationsContext);
 	const { data } = useTokens();
 
-	const options = useTokenOptions(token, data);
+	const error = getMintFormError(mintAmount1, mintAmount2);
 
 	return (
-		<DarkBox>
-			<Grid container alignItems="center">
-				<Grid item xs>
-					<TokenSelector value={token} options={options} onChange={(token) => setToken(token)} />
-				</Grid>
-				{token && (
-					<StyledInputContainer item xs>
-						<Grid container justify="flex-end" alignItems="center">
-							<NumericInput
-								inputProps={{
-									'aria-label': 'mint amount',
-								}}
-								value={mintAmount}
-								handleChange={(change) => setMintAmount(change)}
-							/>
-						</Grid>
-					</StyledInputContainer>
-				)}
+		<Grid container>
+			<Grid item>
+				<StyledContainer container direction="column" justify="space-between">
+					{data && (
+						<>
+							<Grid item>
+								<DarkBox>
+									<Grid container alignItems="center">
+										<Grid item xs>
+											<SelectedToken token={data[0]} />
+										</Grid>
+										<StyledInputContainer item xs>
+											<Grid container justify="flex-end" alignItems="center">
+												<NumericInput
+													inputProps={{
+														'aria-label': 'mint amount 1',
+													}}
+													value={mintAmount1}
+													handleChange={(change) => setMintAmount1(change)}
+												/>
+											</Grid>
+										</StyledInputContainer>
+									</Grid>
+								</DarkBox>
+							</Grid>
+							<Grid item>
+								<DarkBox>
+									<Grid container alignItems="center">
+										<Grid item xs>
+											<SelectedToken token={data[1]} />
+										</Grid>
+										<StyledInputContainer item xs>
+											<Grid container justify="flex-end" alignItems="center">
+												<NumericInput
+													inputProps={{
+														'aria-label': 'mint amount 2',
+													}}
+													value={mintAmount2}
+													handleChange={(change) => {
+														setMintAmount2(change);
+													}}
+												/>
+											</Grid>
+										</StyledInputContainer>
+									</Grid>
+								</DarkBox>
+							</Grid>
+						</>
+					)}
+				</StyledContainer>
 			</Grid>
-		</DarkBox>
+			<Grid item xs={12}>
+				<Button
+					variant="contained"
+					color="secondary"
+					fullWidth
+					disableElevation
+					disabled={(!mintAmount1 && !mintAmount2) || !!error}
+					onClick={() => {
+						dispatch({
+							type: ActionTypes.OPEN_SUCCESS,
+							payload: {
+								title: `Success!`,
+								icon: data?.[0].icon || '',
+								text: `Received ${mintAmount1} ${data?.[0].symbol}`,
+								link: '0xb7d91c4........fa84fc5e6f',
+								buttonText: 'Go Back',
+							},
+						});
+					}}
+				>
+					{error ? error : 'Mint'}
+				</Button>
+			</Grid>
+		</Grid>
 	);
 };
