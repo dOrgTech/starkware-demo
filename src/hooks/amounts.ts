cb00e355ac88ad6648c9d41b438d9231e6fe8408
Elio <eliobricenov@gmail.com>
2021-05-18 23:08:46 -0400

    chore: add amount hooks

---
+++ src/hooks/amounts.ts
@@ -0,0 +1,35 @@
+import { ConversionRate, Token, TokenBalance } from 'models/Token';
+import { useTokenBalances } from '../services/API/token/hooks/useTokenBalances';
+
+export const useConversionError = (
+	token?: Token,
+	amount?: string,
+	limit?: string,
+): string | undefined => {
+	if (!token) return 'Select a token';
+	if (!amount) return 'Enter an amount';
+
+	// TODO: implement big number once we have the API
+	if (limit && Number(amount) > Number(limit)) {
+		return `Insufficient ${token.symbol}`;
+	}
+
+	return undefined;
+};
+
+export const useBalance = (token?: Token): TokenBalance | undefined => {
+	const { data: tokenBalances } = useTokenBalances();
+	return tokenBalances?.find((tokenBalance) => tokenBalance.token.symbol === token?.symbol);
+};
+
+export const useConversionRates = (fromToken?: Token, toToken?: Token): ConversionRate => {
+	if (!fromToken || !toToken) {
+		return { from: 1, to: 1 };
+	}
+
+	// TODO: implement big number once we have the API
+	return {
+		from: Number(toToken.price) / Number(fromToken.price),
+		to: Number(fromToken.price) / Number(toToken.price),
+	};
+};
