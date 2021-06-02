e1cd6c4071b125688b6037fe7c50cf19ce785515
Elio <eliobricenov@gmail.com>
2021-06-01 22:36:36 -0400

    wip: cors workaround

--- src/services/API/utils/http.ts
+++ src/services/API/utils/http.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const httpClient = axios.create({
-	baseURL: 'https://external.integration.starknet.io/',
+	baseURL: 'https://cors-anywhere.herokuapp.com/https://external.integration.starknet.io/',
 });
