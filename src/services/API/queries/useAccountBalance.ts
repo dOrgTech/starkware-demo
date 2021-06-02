a857b1d2754c8f8ed088dedf34f7c6ce69f45a73
Elio <eliobricenov@gmail.com>
2021-06-02 13:29:48 -0400

    fix: disable re-fetch on mount

--- src/services/API/queries/useAccountBalance.ts
+++ src/services/API/queries/useAccountBalance.ts
@@ -15,26 +15,34 @@ export const useAccountBalance = (blockId?: string) => {
 		state: { userId },
 	} = React.useContext(UserContext);
 
-	return useQuery(['accountBalance', blockId], async () => {
-		const balances = new Map<string, string>();
+	return useQuery(
+		['accountBalance', userId, blockId],
+		async () => {
+			const balances = new Map<string, string>();
 
-		const balancesInformation = await Promise.all(
-			tokens.map(({ id }) =>
-				callContract<BalancesFetchResult>({
-					contract_address: contractAddress,
-					blockId: blockId || null,
-					calldata: [userId, id],
-					entry_point_selector: '0x37cad3b31fe9762e3caec6c2649772a660084f23004801193311ff36dc54fe',
-				}),
-			),
-		);
+			const balancesInformation = await Promise.all(
+				tokens.map(({ id }) =>
+					callContract<BalancesFetchResult>({
+						contract_address: contractAddress,
+						blockId: blockId || null,
+						calldata: [userId, id],
+						entry_point_selector:
+							'0x37cad3b31fe9762e3caec6c2649772a660084f23004801193311ff36dc54fe',
+					}),
+				),
+			);
 
-		for (let index = 0; index < balancesInformation.length; index++) {
-			const token = tokens[index];
-			const { data: balanceInformation } = balancesInformation[index];
-			balances.set(token.id, String(balanceInformation.result[0]));
-		}
+			for (let index = 0; index < balancesInformation.length; index++) {
+				const token = tokens[index];
+				const { data: balanceInformation } = balancesInformation[index];
+				balances.set(token.id, String(balanceInformation.result[0]));
+			}
 
-		return balances;
-	});
+			return balances;
+		},
+		{
+			refetchOnMount: false,
+			retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 60000),
+		},
+	);
 };
