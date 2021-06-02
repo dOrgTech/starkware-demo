9a99800a59580fc0c3f2f67276bbe1f59a3afa71
Elio <eliobricenov@gmail.com>
2021-06-01 22:36:18 -0400

    refactor: remove unnecessary variable

--- src/services/API/utils/sendTransaction.ts
+++ src/services/API/utils/sendTransaction.ts
@@ -7,15 +7,10 @@ export const sendTransaction = async ({
 	entry_point_selector,
 	type,
 }: TransactionArgs) => {
-	const data = await httpClient.post<TransactionArgs, TransactionResponse>(
-		`gateway/add_transaction`,
-		{
-			calldata,
-			contract_address,
-			entry_point_selector,
-			type,
-		},
-	);
-
-	return data;
+	return httpClient.post<TransactionArgs, TransactionResponse>(`gateway/add_transaction`, {
+		calldata,
+		contract_address,
+		entry_point_selector,
+		type,
+	});
 };
