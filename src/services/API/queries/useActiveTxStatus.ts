03c50b8e26705d3d41b49f95ed5d7756d615aa29
Elio <eliobricenov@gmail.com>
2021-06-03 18:13:56 -0400

    feat: display tx notifications messages

--- src/services/API/queries/useActiveTxStatus.ts
+++ src/services/API/queries/useActiveTxStatus.ts
@@ -7,6 +7,7 @@ import {
 import { useQuery } from 'react-query';
 import { TransactionStatus } from '../types';
 import { httpClient } from '../utils/http';
+import { useTxNotifications } from '../../../hooks/notifications';
 
 export const useActiveTxStatus = () => {
 	const {
@@ -15,6 +16,62 @@ export const useActiveTxStatus = () => {
 	} = useContext(UserContext);
 	const { dispatch: notificationDispatch } = useContext(NotificationsContext);
 	const [stop, setStop] = useState(false);
+	const { showSuccess, showError, closeSnackbar } = useTxNotifications();
+
+	const displayNotifications = () => {
+		if (!activeTransaction) return;
+
+		if (activeTransaction.type === TransactionType.MINT) {
+			const { mint1 } = activeTransaction.args;
+			notificationDispatch({
+				type: NotificationsActionTypes.OPEN_SUCCESS,
+				payload: {
+					title: `Success!`,
+					icon: mint1.token.icon,
+					text: `Received ${mint1.amount} ${mint1.token.symbol}`,
+					link: '0xb7d91c4........fa84fc5e6f',
+					buttonText: 'Go Back',
+				},
+			});
+		}
+
+		if (activeTransaction.type === TransactionType.SWAP) {
+			const { to } = activeTransaction.args;
+			notificationDispatch({
+				type: NotificationsActionTypes.OPEN_SUCCESS,
+				payload: {
+					title: `Success!`,
+					icon: to.token.icon,
+					text: `Received ${to.amount} ${to.token.symbol}`,
+					link: '0xb7d91c4........fa84fc5e6f',
+					buttonText: 'Go Back',
+				},
+			});
+		}
+	};
+
+	const handleTxSuccess = (data: unknown) => {
+		//TODO: remove execution on rejected
+		if (data === TransactionStatus.REJECTED || data === TransactionStatus.CONFIRMED) {
+			setStop(true);
+			closeSnackbar();
+			showSuccess();
+			userDispatch({
+				type: UserActionTypes.UNSET_ACTIVE_TRANSACTION,
+			});
+			displayNotifications();
+		}
+	};
+
+	const handleTxError = (error: unknown) => {
+		console.error(`Error while querying for Tx ID ${activeTransaction?.id}: ${error}`);
+		setStop(true);
+		closeSnackbar();
+		showError();
+		userDispatch({
+			type: UserActionTypes.UNSET_ACTIVE_TRANSACTION,
+		});
+	};
 
 	const queryProps = useQuery(
 		['txStatus', activeTransaction],
@@ -27,51 +84,8 @@ export const useActiveTxStatus = () => {
 			return data;
 		},
 		{
-			onSuccess: (data) => {
-				if (data === TransactionStatus.REJECTED || data === TransactionStatus.CONFIRMED) {
-					setStop(true);
-					userDispatch({
-						type: UserActionTypes.UNSET_ACTIVE_TRANSACTION,
-					});
-
-					if (!activeTransaction) return;
-
-					if (activeTransaction.type === TransactionType.MINT) {
-						const { mint1 } = activeTransaction.args;
-						notificationDispatch({
-							type: NotificationsActionTypes.OPEN_SUCCESS,
-							payload: {
-								title: `Success!`,
-								icon: mint1.token.icon,
-								text: `Received ${mint1.amount} ${mint1.token.symbol}`,
-								link: '0xb7d91c4........fa84fc5e6f',
-								buttonText: 'Go Back',
-							},
-						});
-					}
-
-					if (activeTransaction.type === TransactionType.SWAP) {
-						const { to } = activeTransaction.args;
-						notificationDispatch({
-							type: NotificationsActionTypes.OPEN_SUCCESS,
-							payload: {
-								title: `Success!`,
-								icon: to.token.icon,
-								text: `Received ${to.amount} ${to.token.symbol}`,
-								link: '0xb7d91c4........fa84fc5e6f',
-								buttonText: 'Go Back',
-							},
-						});
-					}
-				}
-			},
-			onError: (error) => {
-				console.error(`Error while querying for Tx ID ${activeTransaction?.id}: ${error}`);
-				setStop(true);
-				userDispatch({
-					type: UserActionTypes.UNSET_ACTIVE_TRANSACTION,
-				});
-			},
+			onSuccess: handleTxSuccess,
+			onError: handleTxError,
 			enabled: !!activeTransaction,
 			refetchInterval: stop ? false : 10000,
 			refetchIntervalInBackground: true,
