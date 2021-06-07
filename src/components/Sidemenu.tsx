af253710801a2e44d99f1b467c16ac5151a6f9f9
Elio <eliobricenov@gmail.com>
2021-06-07 17:03:49 -0400

    feat: add missing sidebar border and missing links

--- src/components/Sidemenu.tsx
+++ src/components/Sidemenu.tsx
@@ -2,17 +2,20 @@ import { Grid, Link, styled, Typography } from '@material-ui/core';
 import Star from 'assets/menu/Star.svg';
 import Block from 'assets/menu/Block.svg';
 import Document from 'assets/menu/Document.svg';
-import Download from 'assets/menu/Download.svg';
+import Github from 'assets/menu/Github.svg';
 import Starkware from 'assets/menu/Starkware.svg';
+import { ReactComponent as Collaboration } from 'assets/menu/Collaboration.svg';
 
 import hexToRgba from 'hex-to-rgba';
 import React from 'react';
 import { StarknetLogo } from './common/StarknetLogo';
+import { EXPLORER_URL } from '../constants';
 
 const StyledContainer = styled(Grid)({
 	height: '100%',
 	width: 277,
 	background: hexToRgba('#535387', 0.28),
+	borderRight: '1px solid rgba(145, 145, 183, 0.26)',
 });
 
 interface MenuLink {
@@ -37,10 +40,10 @@ const MenuIcon = styled('img')({
 
 const links: MenuLink[] = [
 	{ label: 'What is StarkNet?', icon: Star, url: '/' },
-	{ label: 'Block Explorer', icon: Block, url: '/' },
-	{ label: 'Documentation', icon: Document, url: '/' },
-	{ label: 'Downloads', icon: Download, url: '/' },
-	{ label: 'StarkWare', icon: Starkware, url: '/' },
+	{ label: 'Block Explorer', icon: Block, url: EXPLORER_URL },
+	{ label: 'Documentation', icon: Document, url: 'http://cairo-lang.org/docs/hello_starknet/' },
+	{ label: 'See the Code', icon: Github, url: 'https://github.com/dOrgTech/starkware-demo/' },
+	{ label: 'StarkWare', icon: Starkware, url: 'https://starkware.co/' },
 ];
 
 const MenuItems = styled(Grid)({
@@ -52,21 +55,34 @@ const LogoContainer = styled(Grid)({
 	height: 115,
 });
 
+const CollaborationContainer = styled(MenuItem)({
+	marginBottom: 24,
+});
+
 export const Sidemenu: React.FC = () => {
 	return (
-		<StyledContainer container direction="column" alignItems="center">
-			<LogoContainer item container justify="center" alignItems="center">
-				<StarknetLogo />
-			</LogoContainer>
-			<MenuItems item container spacing={5} direction="column" alignItems="center">
-				{links.map(({ label, icon, url }, i) => (
-					<MenuItem item key={`menulink-${i}`}>
-						<Link href={url}>
-							<MenuIcon src={icon} />
-							<MenuItemText variant="h5">{label}</MenuItemText>
-						</Link>
-					</MenuItem>
-				))}
+		<StyledContainer container direction="column" alignItems="center" justify="space-between">
+			<Grid item container>
+				<LogoContainer item container justify="center" alignItems="center">
+					<StarknetLogo />
+				</LogoContainer>
+				<MenuItems item container spacing={5} direction="column" alignItems="center">
+					{links.map(({ label, icon, url }, i) => (
+						<MenuItem item key={`menulink-${i}`}>
+							<Link href={url} target="_blank" rel="noreferrer">
+								<MenuIcon src={icon} />
+								<MenuItemText>{label}</MenuItemText>
+							</Link>
+						</MenuItem>
+					))}
+				</MenuItems>
+			</Grid>
+			<MenuItems item container direction="column" alignItems="center">
+				<CollaborationContainer item container alignItems="center">
+					<Link href={'https://www.dorg.tech/#/'} target="_blank" rel="noreferrer">
+						<Collaboration />
+					</Link>
+				</CollaborationContainer>
 			</MenuItems>
 		</StyledContainer>
 	);
